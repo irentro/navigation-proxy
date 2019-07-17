936e04859d7999b4ac9313ff90d856691b09dba5
Shane Zhao <s.x.zhao@gmail.com>
2019-07-16 17:28:42 -0700

    updated rendering of proxy modules

--- server/index.js
+++ server/index.js
@@ -30,11 +30,6 @@ app.all('/recommendations/unsave', (req, res) => {
   proxy.web(req, res, {target: 'http://127.0.0.1:4001', changeOrigin: true})
 });
 
-// app.all('/app2', (req, res) => {
-//   console.log('redirecting to app2');
-//   proxy.web(req, res, {target: 'http://127.0.0.1:3002'})
-// });
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
