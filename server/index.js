54e777812eeee5e5d6bd279fdb8096a0819d5a7b
Shane Zhao <s.x.zhao@gmail.com>
2019-07-16 22:05:05 -0700

    added routes to modules

--- server/index.js
+++ server/index.js
@@ -10,24 +10,49 @@ app.use(cors());
 
 const proxy = httpProxy.createProxyServer({});
 
+//Route to recommendations module
 app.all('/app1', (req, res) => {
-  console.log('redirecting to  the app1');
+  console.log('redirecting to recommendations static');
   proxy.web(req, res, {target: 'http://127.0.0.1:4001', changeOrigin: true})
 });
 
 app.all('/recommendations', (req, res) => {
-  console.log('redirecting to  the app1');
+  console.log('redirecting to recommendations');
   proxy.web(req, res, {target: 'http://127.0.0.1:4001', changeOrigin: true})
 });
 
 app.all('/recommendations/save', (req, res) => {
-  console.log('redirecting to  the app1');
+  console.log('redirecting to recommendations save');
   proxy.web(req, res, {target: 'http://127.0.0.1:4001', changeOrigin: true})
 });
 
 app.all('/recommendations/unsave', (req, res) => {
-  console.log('redirecting to  the app1');
+  console.log('redirecting to recommendations unsave');
   proxy.web(req, res, {target: 'http://127.0.0.1:4001', changeOrigin: true})
+});
+
+
+//Route to checkout module
+app.all('/listings', (req, res) => {
+  console.log('redirecting to checkout listings');
+  proxy.web(req, res, {target: 'http://127.0.0.1:3000', changeOrigin: true})
+});
+
+app.all('/bookings', (req, res) => {
+  console.log('redirecting to checkout bookings');
+  proxy.web(req, res, {target: 'http://127.0.0.1:3000', changeOrigin: true})
+});
+
+app.all('/bookings/available/:listingId', (req, res) => {
+  console.log('redirecting to listingID');
+  proxy.web(req, res, {target: 'http://127.0.0.1:3000', changeOrigin: true})
+});
+
+
+//Route to gallery module
+app.all('/gallery/:listingid', (req, res) => {
+  console.log('redirecting to gallery listingId');
+  proxy.web(req, res, {target: 'http://127.0.0.1:3009', changeOrigin: true})
 });
 
 app.listen(port, () => {
